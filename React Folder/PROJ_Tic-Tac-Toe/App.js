f05e0a4605be15e9d0c29cb9fd4fcb45e9d8bfdf
Wynn Greene <8239614+wynngreene@users.noreply.github.com>
2024-08-26 17:24:20 -0600

    Update App.js
    Passing data thru props

--- React Folder/PROJ_Tic-Tac-Toe/App.js
+++ React Folder/PROJ_Tic-Tac-Toe/App.js
@@ -1,20 +1,24 @@
-export default function Square() {
+function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
